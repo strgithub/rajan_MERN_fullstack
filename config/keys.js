54dcd06ed4748d56ed405a9b07394f4734f40e64
st_rajan <st_rajan@hotmail.com>
2021-01-17 12:12:18 +0530

    Version 1.0.0

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+
+        mongoURI: 'mongodb+srv://rajan01:rajan01@clusterstr.kaqfj.mongodb.net/mern_shopping?retryWrites=true&w=majority'
+}
---
+++ routes/api/items.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+
+// Item Model
+const Item = require('../../models/Item');
+
+// @route GET Request api/items
+// @desc Get All Items
+// @access  Public (latter we can add Authoristion)
+router.get('/', (req, res) => {
+    Item.find()
+    .sort({ date: -1})
+    .then(items => res.json(items));
+});
+
+// @route POST api/items
+// 2desc Create A Item
+// @access Public
+router.post('/', (req, res) => {
+    const newItem = new Item({
+        name: req.body.name
+    });
+
+    newItem.save().then(item => res.json(item));
+})
+
+// @route DELETE api/items/:id
+// @desc Delete A Item
+// @access Public
+router.delete('/:id', (req, res) => {
+    Item.findById(req.params.id)
+    .then(item => item.remove().then(() => res.json({success: true})))
+    .catch(err => res.status(404).json({success: false}));
+    });
+
+    
+module.exports = router;
+
